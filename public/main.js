231265f7e1230506f9ca9f9fb05a7bb3e5bcb42e
Zivi Weinstock <zivi.weinstock@gmail.com>
2017-07-27 14:47:08 -0700

    Organize js for legibility, mobile layout improvements.

--- public/main.js
+++ public/main.js
@@ -42,54 +42,59 @@ function createZeroResultsView(searchTerm) {
 function createResultsListView(response) {
   for (let i = 0; i < response.results.length; i += 1) {
     var company = response.results[i];
-    var resultLi = document.createElement('li');
-    var newDiv = document.createElement('div');
-    var webUrl = document.createElement('a');
-    var imgDiv = document.createElement('div');
-    var infoDiv = document.createElement('div');
-    var avatarImg = document.createElement('img');
-    var telLink = document.createElement('a');
-    var laborUl = document.createElement('ul');
+    var resultLi = createEl('li');
+    var newDiv = createEl('div');
+    var webUrl = createEl('a');
+    var imgDiv = createEl('div');
+    var infoDiv = createEl('div');
+    var avatarImg = createEl('img');
+    var telLink = createEl('a');
+    var laborUl = createEl('ul');
 
+    // Create the companies name list
     resultLi.className = 'search-result';
     resultLi.innerText = company.name;
+    searchResults.appendChild(resultLi);
 
+    // Create container with the company information that's shown on company name list click
     newDiv.className = 'company-info hide';
+    resultLi.appendChild(newDiv);
 
-    webUrl.href = company.website;
-    webUrl.innerText = company.website;
-
+    // Split the company info section into two side by side sections
     imgDiv.className = 'image-container';
     infoDiv.className = 'info-container';
 
     avatarImg.src = company.avatarUrl;
     avatarImg.className = 'company-image';
-
-    // Remove (, ), - and space from telephone number so number can be called on link tap
-    telLink.href = `tel:+1${company.phone.replace(/[()\s-]/g, '')}`;
-    telLink.innerText = company.phone;
-    telLink.className = 'company-tel';
+    imgDiv.appendChild(avatarImg);
 
     laborUl.className = 'labor-types';
-    laborUl.innerText = 'Labor Type(s):';
-
+    infoDiv.innerText = 'Labor Type(s):';
     for (let j = 0; j < company.laborType.length; j += 1) {
       var newLaborLi = document.createElement('li');
       newLaborLi.className = 'labor-type';
       newLaborLi.innerText = (company.laborType[j]);
       laborUl.appendChild(newLaborLi);
     }
-
-    imgDiv.appendChild(avatarImg);
-    imgDiv.appendChild(webUrl);
-    infoDiv.appendChild(telLink);
     infoDiv.appendChild(laborUl);
+
+    webUrl.href = company.website;
+    webUrl.innerText = company.website;
+    infoDiv.appendChild(webUrl);
+
+    // Remove (, ), - and space from telephone number so number can be called on link tap
+    telLink.href = `tel:+1${company.phone.replace(/[()\s-]/g, '')}`;
+    telLink.innerText = company.phone;
+    telLink.className = 'company-tel';
+    infoDiv.appendChild(telLink);
+
     newDiv.appendChild(imgDiv);
     newDiv.appendChild(infoDiv);
-    resultLi.appendChild(newDiv);
-
-    searchResults.appendChild(resultLi);
   }
+}
+
+function createEl(type) {
+  return document.createElement(type);
 }
 
 // Events on select of search result list item
@@ -98,15 +103,15 @@ document.querySelector('.search-results').addEventListener('click', function(eve
 })
 
 function createPageableView(response, startPosition, searchTerm) {
+  startPosition = +startPosition;
   var numPages = Math.ceil(response.total / 10);
   var pageNumber = Math.floor(startPosition / 10) + 1;
-
 
   var previousButton = document.createElement('button');
   previousButton.innerText = 'Previous';
   if (startPosition > 0) {
     previousButton.className = 'results-nav results-previous';
-    previousButton.dataset.start = +startPosition - 10;
+    previousButton.dataset.start = startPosition - 10;
     previousButton.dataset.searchTerm = searchTerm;
   } else {
     // Show disabled previousButton when on the first page
@@ -123,7 +128,7 @@ function createPageableView(response, startPosition, searchTerm) {
   nextButton.innerText = 'Next';
   if (response.total - startPosition > 10) {
     nextButton.className = 'results-nav results-next';
-    nextButton.dataset.start = +startPosition + 10;
+    nextButton.dataset.start = startPosition + 10;
     nextButton.dataset.searchTerm = searchTerm;
   } else {
     // Show disabled nextButton when on the last page
@@ -136,12 +141,8 @@ document.querySelector('.results-pageable').addEventListener('click', function(e
   if (event.target.dataset.start) {
     callApi(event.target.dataset.searchTerm, event.target.dataset.start);
   }
-})
+});
 
-
-// todos: view more results button
+// todos:
 // labor type filters
-// responsive view
-
-
 // collapse listing on click close
